423a43fabcca7ed9a17ea637b25bfdcc18bb5182
Manivel-ideas2it <manivel.subramani@ideas2it>
2023-02-03 19:35:54 +0530

    edit on process

--- todo.js
+++ todo.js
@@ -1,7 +1,15 @@
+let input = document.getElementById("text-list");
+input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        add();
+    }
+});
+
 function add(){
     let list = document.createElement("li");
     let remove = document.createElement("button");
     let check =document.createElement("input");
+    let text = document.createElement("button");
     let task =list.innerText = document.getElementById("text-list").value;
     if(task == ""){
         alert("you need to write something...");
@@ -15,6 +23,10 @@ function add(){
     remove.setAttribute("id","button");
     remove.innerText = "remove";
     remove.setAttribute("onclick",'removelist(this)');
+    list.appendChild(text);
+    text.setAttribute("class","text");
+    text.innerText = "edit";
+    text.setAttribute("onclick",'changeContent(this)');
     document.getElementById("task-info").appendChild(list);
     document.getElementById("text-list").value ='';
 }
@@ -27,17 +39,24 @@ function removelist(element){
 }
 
 function checkList(element){
-    if(element.checked){
-        console.log(element.parentNode);
-        element.style.backgroundColor = "blue";
-        completedList(element.parentNode);
+    let countValue = document.getElementById("task-status").childElementCount -1;
+    let listTwo = document.getElementById("task-status");
+    if(element.checked){  
+        listTwo.appendChild(element.parentNode);
+        countValue++;
     } else{
         document.getElementById("task-info").appendChild(element.parentNode);
+        countValue--;
+    }
+    if(countValue == 0){
+         document.getElementById("name").style.display = "none";
+    } else {
+        document.getElementById("name").style.display = "block";
+        listTwo.style.display = "block"
     }
 }
 
-function completedList(element){
-    let listTwo = document.getElementById("task-status");
-    listTwo.style.visibility = "visible";
-    listTwo.appendChild(element); 
+function changeContent(element){
+    console.log(element.nextElementSibling);
 }
+
